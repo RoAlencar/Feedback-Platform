0e770ce9d08d0cb202d3c957edac8f4c57fbe300
novaes1998 <lucas.novaes1109@gmail.com>
2026-04-22 23:34:01 -0300

    feat: implement WeeklyReport domain validations

---
+++ shared-lib/src/main/java/br/com/fiap/shared/domain/exception/AverageScoreException.java
@@ -0,0 +1,7 @@
+package br.com.fiap.shared.domain.exception;
+
+public class AverageScoreException extends RuntimeException {
+    public AverageScoreException(String message) {
+        super(message);
+    }
+}
---
+++ shared-lib/src/main/java/br/com/fiap/shared/domain/exception/TotalFeedbacksException.java
@@ -0,0 +1,7 @@
+package br.com.fiap.shared.domain.exception;
+
+public class TotalFeedbacksException extends RuntimeException {
+    public TotalFeedbacksException(String message) {
+        super(message);
+    }
+}
