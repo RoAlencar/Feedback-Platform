1916630df6f03a27ce1cab8ef99e5bf697d431d8
novaes1998 <lucas.novaes1109@gmail.com>
2026-04-23 17:04:22 -0300

    feat: add DailyReportItem mapper

---
+++ analytics-function/src/main/java/br/com/fiap/analytics/adapter/output/persistence/mapper/DailyReportItemMapper.java
@@ -0,0 +1,34 @@
+package br.com.fiap.analytics.adapter.output.persistence.mapper;
+
+import br.com.fiap.analytics.adapter.output.persistence.entity.DailyReportItemJpaEntity;
+import br.com.fiap.analytics.adapter.output.persistence.entity.WeeklyReportJpaEntity;
+import br.com.fiap.shared.domain.entity.DailyReportItem;
+
+import java.util.UUID;
+
+public class DailyReportItemMapper {
+
+    private static DailyReportItemJpaEntity toJpaEntity(
+            DailyReportItem domain,
+            WeeklyReportJpaEntity weeklyReport
+    ){
+
+        DailyReportItemJpaEntity entity = new DailyReportItemJpaEntity();
+
+        entity.setId(UUID.randomUUID());
+        entity.setWeeklyReport(weeklyReport);
+        entity.setDate(domain.getDate());
+        entity.setFeedbackCount(domain.getFeedbackCount());
+
+        return entity;
+    }
+
+
+    public static DailyReportItem toDomain(DailyReportItemJpaEntity entity){
+
+        return new DailyReportItem(
+                entity.getDate(),
+                entity.getFeedbackCount()
+        );
+    }
+}
