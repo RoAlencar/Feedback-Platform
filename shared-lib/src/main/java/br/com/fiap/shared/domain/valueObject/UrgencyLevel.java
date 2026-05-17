97ee89a80536e180554fe42eb0714b7d0125e9a1
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2026-05-17 13:27:18 -0300

    fix(shared-lib): correct UrgencyLevel mapping

--- shared-lib/src/main/java/br/com/fiap/shared/domain/valueObject/UrgencyLevel.java
+++ shared-lib/src/main/java/br/com/fiap/shared/domain/valueObject/UrgencyLevel.java
@@ -6,13 +6,16 @@ public enum UrgencyLevel {
     HIGH,
     CRITICAL;
 
-
     public static UrgencyLevel fromGrade(Score score) {
 
-        if(score.isCritical()) return CRITICAL;
-        if (score.isAttention()) return HIGH;
-        if(score.isWarning()) return MEDIUM;
-        if(score.isElevated()) return LOW;
+        if (score.isCritical())
+            return CRITICAL;
+        if (score.isWarning())
+            return HIGH;
+        if (score.isAttention())
+            return MEDIUM;
+        if (score.isElevated())
+            return LOW;
 
         throw new IllegalArgumentException("Grade fora das faixas esperadas");
     }
